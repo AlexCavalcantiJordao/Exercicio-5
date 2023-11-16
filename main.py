c2a9bcbeeb9c84775ed5a8c65ebe39a272c600ac
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-11-16 12:11:55 -0300

    .idea/.gitignore .idea/ComadosIfElifElse.iml .idea/inspectionProfiles/profiles_settings.xml . idea/misc.xml .idea/modules.xml main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite a sua idade da pessoa: "))
+if(idade > 18):
+    print("É maior de idade !")
+elif(idade > 16):
+    print("É juvenil !")
+else:
+    print("É menor de idade !")
